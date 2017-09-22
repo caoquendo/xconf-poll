752557b244082c16318a02df799cb3b3baf9278f
luz <luzma_87@yahoo.com>
2017-09-22 12:02:50 -0500

    adds logout button

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -5,19 +5,30 @@ import {createContainer} from 'meteor/react-meteor-data';
 import Login from "./components/Login";
 
 class App extends Component {
+    __onClick = (event) => {
+        event.preventDefault();
+
+        Meteor.logout();
+    };
+
     render() {
         return this.props.currentUser ?
-               <div>Hola {this.props.currentUser.username}</div> :
+               <div>
+                   Hola {this.props.currentUser.username}
+                   <button type="submit" onClick={this.__onClick.bind(this)}>
+                       Bye
+                   </button>
+               </div> :
                <Login/>;
     }
 }
 
 App.propTypes = {
-    currentUser: PropTypes.object
+    currentUser : PropTypes.object
 };
 
 export default createContainer(() => {
-   return {
-       currentUser: Meteor.user()
-   };
+    return {
+        currentUser : Meteor.user()
+    };
 }, App);
