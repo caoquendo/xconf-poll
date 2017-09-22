e5067524477f381db94c2796fd0da3c541047085
luz <luzma_87@yahoo.com>
2017-09-21 19:03:41 -0500

    initializes collections with defaults

---
+++ imports/api/conferences.js
@@ -0,0 +1,5 @@
+import {Meteor} from 'meteor/meteor'
+import {Mongo} from 'meteor/mongo';
+
+export const Conferences = new Mongo.Collection('conferences');
+
--- server/main.js
+++ server/main.js
@@ -1,5 +1,19 @@
-import { Meteor } from 'meteor/meteor';
+import {Meteor} from 'meteor/meteor';
+import {Conferences} from '../imports/api/conferences'
 
 Meteor.startup(() => {
-  // code to run on server at startup
+    if (Conferences.find().count() === 0) {
+        JSON.parse(Assets.getText("conferences.json")).conferences.forEach((conference) => {
+            Conferences.insert(conference);
+        });
+    }
+
+    if (Meteor.users.find().count() === 0) {
+        JSON.parse(Assets.getText("users.json")).users.forEach((user) => {
+            Accounts.createUser({
+                username: user.name,
+                password: user.ticketCode
+            });
+        });
+    }
 });
