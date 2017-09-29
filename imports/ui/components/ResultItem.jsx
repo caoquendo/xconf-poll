0770b3ce47d3a5e105042af1cea286be30e91d98
Carlos Andres Oquendo <coquendo@thoughtworks.com>
2017-09-29 08:27:47 -0500

    Adjusts layout of result item.

--- imports/ui/components/ResultItem.jsx
+++ imports/ui/components/ResultItem.jsx
@@ -8,19 +8,23 @@ export default class ResultItem extends Component {
     render() {
         const numWinners = 4;
         const topic = this.props.topic;
-        const className = classnames("card-content", {"pink lighten-4" : this.props.index < numWinners});
+        const className = classnames("card-content topic-content", {"pink lighten-4" : this.props.index < numWinners});
         return (
             <div className="col s12">
                 <div className="card">
                     <div className={className}>
+                        <div className="topic-container">
+                            <div className="topic-for-vote deep-purple-text text-lighten-1">{topic.text}</div>
+                            <div className="topic-context">
+                                <small>
+                                    En relación a&nbsp;
+                                    <span className="pink-text">{topic.conference}</span>
+                                </small>
+                            </div>
+                        </div>
                         <span className="chip right vote-count deep-purple lighten-3">
-                                {topic.votes ? topic.votes : 0} voto{topic.votes === 1 ? '' : 's'}
-                                </span>
-                            <span className="card-title pink-text text-lighten-1">
-                                <h5>{topic.conference}</h5>
-                                </span>
-
-                        {topic.text}
+                            {topic.votes ? topic.votes : 0} voto{topic.votes === 1 ? '' : 's'}
+                        </span>
                     </div>
                 </div>
             </div>
